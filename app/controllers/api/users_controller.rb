2003258a1925c62ee66b9d4a3419dc435c461570
Hannah Jeror <jerorhn@yahoo.com>
2018-04-18 16:27:08 -0400

    Fix bugs

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,13 +2,8 @@ class Api::UsersController < ApiController
   before_action :authenticated?
 
   def index
-    return permission_denied_error unless conditions_met
     users = User.all
     render json: users, each_serializer: UserSerializer
-  end
-
-  private conditions_met
-    :authenticated?
   end
 
 end
